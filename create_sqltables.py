bbf8e48a9854093cc180d3b2245308716bddd02e
Kyle Norris <r.kyle.norris@gmail.com>
2024-11-02 18:26:17 -0400

    refactored create tables function to utilize sql script

--- create_sqltables.py
+++ create_sqltables.py
@@ -1,135 +1,40 @@
 import sqlite3
-import prep
 from datetime import datetime
+from string import Template
+from pathlib import Path
 # Connect to SQLite database (or create it)
 
 
 def create_daylio_sql_tables():
+    """
+    function to drop all tables from db, then recreate accordingly
+
+    :return:
+    """
     conn = sqlite3.connect('data/daylio.db')
     cursor = conn.cursor()
+    tables = [
+        'customMoods',
+        'tags',
+        'dayEntries',
+        'goals',
+        'prefs',
+        'tag_groups',
+        'goalEntries',
+        'calendar',
+        'mood_groups',
+        'entry_tags',
+    ]
 
-    for tbl in prep.tables_needed:
-        cursor.execute(f'DROP TABLE IF EXISTS {tbl}')
-    cursor.execute('DROP TABLE IF EXISTS calendar')
-    cursor.execute('DROP TABLE IF EXISTS mood_groups')
-    cursor.execute('DROP TABLE IF EXISTS entry_tags')
-    # Create customMoods table
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS customMoods (
-        id INTEGER PRIMARY KEY,
-        custom_name TEXT,
-        mood_value INTEGER,
-        mood_group_id INTEGER,
-        mood_group_order INTEGER,
-        createdAt DATETIME,
-        date DATE
-    )
-    ''')
+    for table in tables:
+        q_template = Template("DROP TABLE IF EXITS {{table_name}}")
+        query = q_template.substitute(table_name=table)
+        cursor.execute(query)
 
-    # Create tags table
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS tags (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        createdAt DATETIME,
-        `order` INTEGER,
-        id_tag_group INTEGER,
-        date DATE,
-        FOREIGN KEY (id_tag_group) REFERENCES tag_groups(id)
-    )
-    ''')
+    create_tables_path = Path.cwd() / 'sql' / 'create_tables.sql'
 
-    # Create dayEntries table
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS dayEntries (
-        id INTEGER PRIMARY KEY,
-        datetime DATETIME,
-        mood INTEGER,
-        note TEXT,
-        note_title TEXT,
-        date DATE,
-        FOREIGN KEY (mood) REFERENCES customMoods(id)
-    )
-    ''')
+    cursor.execute(create_tables_path.read_text())
 
-    # Create goalEntries table
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS goalEntries (
-        id INTEGER PRIMARY KEY,
-        goalId INTEGER,
-        createdAt DATETIME,
-        date DATE,
-        FOREIGN KEY (goalId) REFERENCES goals(goal_id)
-    )
-    ''')
-
-    # Create goals table
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS goals (
-        id INTEGER PRIMARY KEY,
-        goal_id INTEGER,
-        created_at DATETIME,
-        id_tag INTEGER,
-        end_date DATEIME,
-        name TEXT,
-        note TEXT,
-        date DATE,
-        date_end Date,
-        FOREIGN KEY (id_tag) REFERENCES tags(id)
-    )
-    ''')
-
-    # Create prefs table
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS prefs (
-        AUTO_BACKUP_IS_ON BOOLEAN,
-        LAST_DAYS_IN_ROWS_NUMBER INTEGER,
-        DAYS_IN_ROW_LONGEST_CHAIN INTERGER,
-        LAST_ENTRY_CREATION_TIME DATETIME
-    )
-    ''')
-
-    # Create tag_groups table
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS tag_groups (
-        id INTEGER PRIMARY KEY,
-        name TEXT
-    )
-    ''')
-
-    # create calendar table
-    cursor.execute('''
-    CREATE TABLE calendar (
-        TimeStamp DATETIME PRIMARY KEY,
-        Date DATE,
-        Day INTEGER,
-        DayName TEXT,
-        Week INTEGER,
-        Month INTEGER,
-        MonthName TEXT,
-        Quarter INTEGER,
-        Year INTEGER,
-        IsWeekend BOOLEAN,
-        IsWeekday BOOLEAN
-    );
-    ''')
-
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS mood_groups (
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            value INTEGER
-        
-        );
-        ''')
-
-    cursor.execute('''
-    CREATE TABLE entry_tags (
-    entry_id INTEGER,
-    tag TEXT,
-    FOREIGN KEY (entry_id) REFERENCES dayEntries(id)
-);
-    ''')
     conn.commit()
     conn.close()
 
