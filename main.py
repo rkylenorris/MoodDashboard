13b8ad167ab6cce9f424085a8ddd158bbe5d7fa6
Kyle Norris <r.kyle.norris@gmail.com>
2024-11-03 10:23:19 -0500

    added in create_sql_tables stuff

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from pickup_data import DaylioDataPrep
 from clean_data import DaylioTable, get_table_info, create_mood_groups, create_entry_tags
 from create_calendar import write_calendar_to_db
+from create_sqltables import create_daylio_sql_tables, insert_prefs
 from pathlib import Path
 import json
 import pandas as pd
@@ -14,7 +15,7 @@ prep.extract_data()
 prep.decode_backup()
 
 # section for initializing db and creating tables
-# TODO: add this section in
+create_daylio_sql_tables()
 
 # section for loading extracted json data into class objects
 data_dir = Path.cwd() / 'data'
@@ -25,15 +26,18 @@ tables = (data_dir / 'tables_needed.txt').read_text().split('\n')
 daylio_tables = []
 
 for table in tables:
-    df = pd.DataFrame(daylio_data[table])
-    column_info = get_table_info(table)
-    daylio_table = DaylioTable(table, df, column_info)
-    daylio_tables.append(daylio_table)
-    if daylio_table.name == 'dayEntries':
-        columns = get_table_info('entry_tags')
-        daylio_tables.append(
-            create_entry_tags(daylio_table, columns)
-        )
+    if table == 'prefs':
+        continue
+    else:
+        df = pd.DataFrame(daylio_data[table])
+        column_info = get_table_info(table)
+        daylio_table = DaylioTable(table, df, column_info)
+        daylio_tables.append(daylio_table)
+        if daylio_table.name == 'dayEntries':
+            columns = get_table_info('entry_tags')
+            daylio_tables.append(
+                create_entry_tags(daylio_table, columns)
+            )
 
 mood_groups_columns = get_table_info('mood_groups')
 daylio_tables.append(
@@ -46,6 +50,8 @@ db_conn = sqlite3.connect(str(db_path))
 
 for table in daylio_tables:
     table.to_sql(db_conn)
+
+insert_prefs(daylio_data['prefs'])
 
 write_calendar_to_db(db_conn)
 
